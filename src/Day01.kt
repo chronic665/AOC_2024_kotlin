df1aea79fb7837a136e3d6abab95c6dc7e198943
chronic <chronic6666@gmail.com>
2024-12-03 20:54:12 +0000

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,44 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun readInputLine(line: String): Pair<Int, Int> = line
+        .split(" ")
+        .let { Pair(it.first(), it.last())}
+        .let { Pair(it.first.toInt(), it.second.toInt()) }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        input
+            .map { readInputLine(it) }
+            .forEach {
+                firstList.add(it.first)
+                secondList.add(it.second)
+            }
+        firstList.sort()
+        secondList.sort()
+
+        return firstList.zip(secondList)
+            .sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val destinationCounts = mutableMapOf<Int, Int>()
+        val locationIds = input
+            .map { readInputLine(it) }
+            .onEach { destinationCounts.merge(it.second, 1, Int::plus) }
+            .map { it.first }
+
+        return locationIds
+            .map { it * (destinationCounts[it] ?: 0) }
+            .sum()
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
